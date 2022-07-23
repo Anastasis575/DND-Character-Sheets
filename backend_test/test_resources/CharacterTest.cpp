3a5fbc0de13f3021418838247d1afd15629f3f72
dimits-exe <manisfatsa@gmail.com>
2022-07-23 13:07:54 +0300

    Keep up with refactor

--- backend_test/test_resources/CharacterTest.cpp
+++ backend_test/test_resources/CharacterTest.cpp
@@ -1,36 +1,53 @@
 #include "pch.h"
-#include "CharacterBuilder.h"
+#include "Character.h"
 
 using namespace DND;
 
 class CharacterTest : public ::testing::Test {
 
 protected:
-	const std::string NAME = "PANOS";
+	const std::string CHAR_NAME = "PANOS";
+	const std::string PLAYER_NAME = "SEX FROG";
 	const StatModifier RACE = StatModifier("LAMROUPOULI", AttributeSet(3)); //todo: make meaningful
 	const StatModifier CLASS = StatModifier("WORKING", AttributeSet(4));
 	const StatModifier SUBCLASS = StatModifier("KOMMOUNISTIS", AttributeSet(5));
 
-	Character character = CharacterBuilder(NAME).build();
+	Character* character = createCharacter();
 
 	virtual void setUp() {
-		character = CharacterBuilder(NAME).setRace(RACE).setClass(CLASS).setSubClass(SUBCLASS).build();
+		createCharacter();
+	}
+
+	virtual void tearDown() {
+		delete character;
+	}
+
+	Character * createCharacter() {
+		character = new Character(CHAR_NAME, PLAYER_NAME);
+	
+		character->setRace(RACE);
+		character->setClass(CLASS);
+		character->setSubClass(SUBCLASS);
+
+		return character;
 	}
 };
 
 TEST_F(CharacterTest, BuilderTest) {
-	EXPECT_EQ(character.getName(), NAME);
+	EXPECT_EQ(character->getPlayerName(), PLAYER_NAME);
+	EXPECT_EQ(character->getCharacterName(), CHAR_NAME);
 
-	EXPECT_EQ(character.getRace().getName(), RACE.getName());
-	EXPECT_EQ(character.getRace().getStats(), RACE.getStats());
+	EXPECT_EQ(character->getRace().getName(), RACE.getName());
+	EXPECT_EQ(character->getRace().getStats(), RACE.getStats());
 
-	EXPECT_EQ(character.getClass().getName(), CLASS.getName());
-	EXPECT_EQ(character.getClass().getStats(), CLASS.getStats());
+	EXPECT_EQ(character->getClass().getName(), CLASS.getName());
+	EXPECT_EQ(character->getClass().getStats(), CLASS.getStats());
 
-	EXPECT_EQ(character.getSubclass().getName(), SUBCLASS.getName());
-	EXPECT_EQ(character.getSubclass().getStats(), SUBCLASS.getStats());
+	EXPECT_EQ(character->getSubclass().getName(), SUBCLASS.getName());
+	EXPECT_EQ(character->getSubclass().getStats(), SUBCLASS.getStats());
 }
 
 TEST_F(CharacterTest, GetAttributeScoreTest) {
-	// EXPECT_EQ(character.getAttributeScore(Attribute::Charisma))
+	//EXPECT_EQ(character->getAttributeScore(Attribute::Charisma)
+		 
 }
