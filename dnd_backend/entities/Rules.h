48b7cb7efa1cc288c1e020574191de1f4d17db18
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-09-03 22:33:51 +0300

    Add generated file

---
+++ dnd_backend/entities/Rules.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+/*File generated automatically by auto-enum.py https://github.com/dimits-exe/CppAutomationTools*/ 
+
+namespace DND {
+	namespace entity_details {
+		int MIN_ATTRIBUTE_VALUE = 0
+		int MAX_ATTRIBUTE_VALUE = 20
+		int MIN_LEVEL = 1
+		int MAX_LEVEL = 20
+		int MIN_HP = 0
+		int MAX_HP = 9999
+		int MIN_AC = -5
+		int MAX_AC = 32
+		int MIN_SPEED = 1
+		int MAX_SPEED = 9999
+		int DEFAULT_LEVEL = 1
+		int DEFAULT_HP = 1
+		int DEFAULT_AC = 0
+		int DEFAULT_SPEED = 0
+	}
+}
