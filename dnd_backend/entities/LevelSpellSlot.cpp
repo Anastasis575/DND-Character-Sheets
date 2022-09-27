f364290e4322440fd14d5823bc9cd61d31ce8d3e
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-09-27 17:53:37 +0300

    Add spell slots logic

---
+++ dnd_backend/entities/LevelSpellSlot.cpp
@@ -0,0 +1,24 @@
+#include "LevelSpellSlot.h"
+#include <array>
+
+using namespace DND;
+using namespace DND::entity_details;
+
+LevelSpellSlot::LevelSpellSlot(const std::array<int, entity_details::MAX_SPELL_LEVEL>& maxSlots) {
+	for (unsigned int i=0; i <= maxSlots.size(); i++) {
+		MAX_SLOTS.setAmount(i, maxSlots[i]);
+		currentSlots.setAmount(i, maxSlots[i]);
+	}
+}
+
+void LevelSpellSlot::refresh() {
+	currentSlots = MAX_SLOTS;
+}
+
+void LevelSpellSlot::spellUsed(int spellLevel) {
+	currentSlots.setAmount(spellLevel, getSpellSlots(spellLevel) - 1);
+}
+
+int LevelSpellSlot::getSpellSlots(int spellLevel) const {
+	return currentSlots.getAmount(spellLevel);
+}
---
+++ dnd_backend/entities/LevelSpellSlot.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "ObjectCounter.h"
+#include "Rules.h"
+
+namespace DND {
+	class LevelSpellSlot {
+	public:
+		/**
+		 * @brief Create a spell slot for a specific character level.
+		 * @param maxSlots an array where maxSlots[spellLevel] = spellSlots for that spell level
+		*/
+		explicit LevelSpellSlot(const std::array<int, entity_details::MAX_SPELL_LEVEL>& maxSlots);
+
+		/**
+		 * @brief Get the spell slots for a spell level.
+		 * @param spellLevel the spell level
+		 * @return how many spell slots remain
+		*/
+		int getSpellSlots(int spellLevel) const;
+
+		/**
+		 * @brief Mark that a spell slot was temporarily consumed.
+		 * @param spellLevel the level of the spell consumed
+		*/
+		void spellUsed(int spellLevel);
+
+		/**
+		 * @brief Reset the spell slots to the max slots.
+		*/
+		void refresh();
+
+
+	private:
+		entity_details::ObjectCounter<int> MAX_SLOTS; // effectively const, non-const because of serialization
+		entity_details::ObjectCounter<int> currentSlots;
+	};
+
+}
+
